fe224f9159e100e38c70b838d34de1ebab00ab33
Cesar Garay <cgaray.aguilar97@gmail.com>
2020-08-25 23:12:34 -0600

    Media queries finished

--- main.js
+++ main.js
@@ -3,3 +3,19 @@ const container = document.querySelector('#container')
 document.querySelector('#button-menu').addEventListener('click', () => {
   container.classList.toggle('active')
 })
+
+const toggleDrawable = () => {
+  if (window.innerWidth <= 768) {
+    container.classList.remove('active')
+  } else {
+    container.classList.add('active')
+  }
+}
+
+window.addEventListener('resize', () => {
+  toggleDrawable()
+})
+
+window.addEventListener('load', () => {
+  toggleDrawable()
+})
